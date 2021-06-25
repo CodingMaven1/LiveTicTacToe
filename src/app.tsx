6719ed45da0efd4b0852a54164948ffb1a397617
Naman <namana019@outlook.com>
2021-06-26 00:31:45 +0530

    Working on the board

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Board from './board/board';
+import Board from './game/game';
 
 import './app.css';
 
---
+++ src/game/game.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './game.css';
+
+interface Props {
+    type: string
+}
+
+const Game : React.FC<Props> = (props) => {
+
+    const [player, setPlayer] = React.useState<string>("Player1");
+    const [moves, setMoves] = React.useState<string[][]>([["","",""],["","",""],["","",""]]);
+
+    const onMoveHandler = (index1: number, index2: number) => {
+        const newmoves = [...moves];
+        if(player === "Player1") {
+            newmoves[index1][index2] = "0";
+            setPlayer("Player2");
+        }
+        else {
+            newmoves[index1][index2] = "X";
+            setPlayer("Player1");
+        }
+        setMoves(newmoves);
+    }
+
+    return (
+        <div className="Game">
+            <div className="Game--Board">
+                {
+                    moves.map((rows, rowid) => {
+                        return (
+                            <div className="Game--BoardRow" key={rowid}>
+                                {
+                                    rows.map((cube, cubeid) => {
+                                        return(
+                                            <div className="Game--BoardCube" key={cubeid} onClick={() => onMoveHandler(rowid, cubeid)}>{cube}</div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Game;
