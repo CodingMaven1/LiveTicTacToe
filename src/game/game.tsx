cbaff74242b38a49862a3d53ae9c7d8fba817512
Naman <namana019@outlook.com>
2021-06-26 18:22:46 +0530

    Result calculation made

--- src/game/game.tsx
+++ src/game/game.tsx
@@ -11,23 +11,36 @@ import './game.css';
 const Game = () => {
 
     const [view, setView] = React.useState<string>("choose");
-    const [player, setPlayer] = React.useState<string>("Player1");
+    const [player, setPlayer] = React.useState<string>("Player");
     const [moves, setMoves] = React.useState<string[][]>([["","",""],["","",""],["","",""]]);
     const [icons, setIcons] = React.useState([alienlogo, clownlogo]);
+    const [champion, setChampion] = React.useState<string | null>(null)
 
     const shapes = [alienlogo, clownlogo, frieslogo, nerdlogo, pumpkinlogo];
 
     const onMoveHandler = (index1: number, index2: number) => {
         const newmoves = [...moves];
-        if(player === "Player1") {
+
+        if(player === "Player") {
             newmoves[index1][index2] = "0";
-            setPlayer("Player2");
+            setMoves(newmoves)
+            const result = checkWinner(moves);
+            if(result !== null) {
+                if(result === 1) {
+                    setChampion("Player")
+                } 
+                else if(result === 0) {
+                    setChampion("Tied")
+                }
+                setView("result")
+            }
+            setPlayer("Computer");
         }
         else {
             newmoves[index1][index2] = "1";
-            setPlayer("Player1");
+            setMoves(newmoves);
+            setPlayer("Player");
         }
-        setMoves(newmoves);
     }
 
     const onShapeHandler = (index: number) => {
@@ -40,6 +53,60 @@ const Game = () => {
         setIcons(newicons);
         setView("board");
     }
+
+    function checkEquality(a:string ,b: string,c: string): boolean {
+        return a === b && b === c && a !== '';
+    }
+
+    function checkWinner(board: string[][]) : number | null {
+        let winner = null;
+        let winnerfound = false;
+        let slotsleft = false;
+
+        // Checking horizontally
+        for(let i=0; i<3; i++) {
+            if(checkEquality(board[i][0], board[i][1], board[i][2])) {
+                winnerfound = true;
+            }
+        }
+
+        //Checking vertically
+        for(let i=0; i<3; i++) {
+            if(checkEquality(board[0][i], board[1][i], board[2][i])) {
+                winnerfound = true;
+            }
+        }
+
+        //Checking diagonally
+        if(checkEquality(board[0][0], board[1][1], board[2][2]) || checkEquality(board[0][2], board[1][1], board[2][0])) {
+            winnerfound = true;
+        }
+
+        //Checking slot availability
+        for(let i=0; i<3; i++){
+            for(let j=0; j<3; j++){
+                if(board[i][j] === '') {
+                    slotsleft = true;
+                    break;
+                }
+            }
+        }
+
+        if(!slotsleft && !winnerfound) {
+            winner = 0;
+        }
+        else if(slotsleft) {
+            if(winnerfound) {
+                winner = player === "Player" ? 1 : -1
+            }
+        }
+
+        return winner
+    }
+
+    // const computerAI = (board: string[][], depth: number, isMaximizing: boolean) => {
+    //     if(isMaximizing)
+    // }
 
     return (
         <div className="Game">
@@ -82,7 +149,11 @@ const Game = () => {
                                 )
                             })
                         }
-                    </div> : null
+                        </div> :
+                            view === "result" ? 
+                                <div className="Game--Result">
+                                    {champion}
+                                </div> : null
             }
         </div>
     )
