62c67fbd65889b5a95ca5b5d3d4d7234f00b4464
Naman <namana019@outlook.com>
2021-06-26 16:03:02 +0530

    Choose option made

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import './app.css';
 const App = () => {
     return (
         <div className="App">
-            <Board type="cross" />
+            <Board />
         </div>
     )
 }
--- src/game/game.tsx
+++ src/game/game.tsx
@@ -7,14 +7,15 @@ import nerdlogo from '../images/nerd.png';
 import pumpkinlogo from '../images/pumpkin.png';
 import './game.css';
 
-interface Props {
-    type: string
-}
 
-const Game : React.FC<Props> = (props) => {
+const Game = () => {
 
+    const [view, setView] = React.useState<string>("choose");
     const [player, setPlayer] = React.useState<string>("Player1");
     const [moves, setMoves] = React.useState<string[][]>([["","",""],["","",""],["","",""]]);
+    const [icons, setIcons] = React.useState([alienlogo, clownlogo]);
+
+    const shapes = [alienlogo, clownlogo, frieslogo, nerdlogo, pumpkinlogo];
 
     const onMoveHandler = (index1: number, index2: number) => {
         const newmoves = [...moves];
@@ -23,31 +24,65 @@ const Game : React.FC<Props> = (props) => {
             setPlayer("Player2");
         }
         else {
-            newmoves[index1][index2] = "X";
+            newmoves[index1][index2] = "1";
             setPlayer("Player1");
         }
         setMoves(newmoves);
     }
 
+    const onShapeHandler = (index: number) => {
+        const newicons = [...icons];
+
+        newicons[0] = shapes[index];
+        shapes.splice(index, 1);
+        newicons[1] = shapes[Math.floor(Math.random() * 4)];
+
+        setIcons(newicons);
+        setView("board");
+    }
+
     return (
         <div className="Game">
-            <div className="Game--Board">
-                {
-                    moves.map((rows, rowid) => {
-                        return (
-                            <div className="Game--BoardRow" key={rowid}>
-                                {
-                                    rows.map((cube, cubeid) => {
-                                        return(
-                                            <div className="Game--BoardCube" key={cubeid} onClick={() => onMoveHandler(rowid, cubeid)}>{cube}</div>
-                                        )
-                                    })
-                                }
-                            </div>
-                        )
-                    })
-                }
-            </div>
+            {
+                view === "choose" ? 
+                    <div className="Game--Choose">
+                        {
+                            shapes.map((shape, shapeid) => {
+                                return (
+                                    <div className="Game--ChooseCube" key={shapeid} onClick={() => onShapeHandler(shapeid)}>
+                                        <img src={shape} className="Game--BoardIcon" alt="shape" />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div> : 
+                    view === "board" ? 
+                        <div className="Game--Board">
+                        {
+                            moves.map((rows, rowid) => {
+                                return (
+                                    <div className="Game--BoardRow" key={rowid}>
+                                        {
+                                            rows.map((cube, cubeid) => {
+                                                return(
+                                                    <div className="Game--BoardCube" key={cubeid} 
+                                                        onClick={() => onMoveHandler(rowid, cubeid)}>
+                                                        {
+                                                            cube === "0" ? 
+                                                                <img src={icons[0]} className="Game--BoardIcon" alt="player1icon" /> : 
+                                                                cube === "1" ?
+                                                                    <img src={icons[1]} className="Game--BoardIcon" alt="player1icon" /> : null
+                                                        }
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                    </div> : null
+            }
         </div>
     )
 }
